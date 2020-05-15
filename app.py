643606f79b2575e14ae5f5abc2b4d0bde6dc5b81
WillCJR <wcranmer@gmail.com>
2020-05-15 22:14:41 +0000

    Random Card Generator Project

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
 import random
 suites = ['♡', '♢', '♤', '♧']
 values = list(range(1,14))
+# high_values = 
 
 def get_random_card():
     # your code here
-    return "3♤"
-    
+    suite = random.choice(suites)
+    value = str(random.choice(values))
+    if value == "11":
+        value = "J"
+    elif value == "12":
+        value = "Q"
+    elif value == "13":
+        value = "K"
+    elif value == "14":
+        value = "A"
+    random_card = suite + str(value)
+    return random_card
+
 print(get_random_card())
